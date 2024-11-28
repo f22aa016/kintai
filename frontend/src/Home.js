2a42378d67a7c24d1b3e0ca66752f94d8e67e9ad
TS-7251922 <f22aa014@chuo.ac.jp>
2024-11-28 10:31:27 +0900

    競合解消v2

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './App.css';
+import './app_style.css';
 import LogoutIcon from '@mui/icons-material/Logout';
 
 function Home() {
